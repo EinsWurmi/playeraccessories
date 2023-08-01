8093482a0ca0a58538098301388e06af588d7245
EinsWurmi <wurmlitv@gmail.com>
2023-08-01 18:35:41 +0200

    Updated ``config update event``

--- api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/events/MilieuUpdateEvent.java
+++ api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/events/MilieuUpdateEvent.java
@@ -6,6 +6,32 @@ import net.labymod.api.event.LabyEvent;
 import net.labymod.api.event.Phase;
 
 @LabyEvent(background = true)
-public record MilieuUpdateEvent(Phase phase, MilieuElement milieu, Object value) implements Event {
+public class MilieuUpdateEvent implements Event {
+
+  private final Phase phase;
+  private final MilieuElement milieu;
+  private Object value;
+
+  public MilieuUpdateEvent(Phase phase, MilieuElement milieu, Object value) {
+    this.phase = phase;
+    this.milieu = milieu;
+    this.value = value;
+  }
+
+  public Phase phase() {
+    return phase;
+  }
+
+  public MilieuElement milieu() {
+    return milieu;
+  }
+
+  public <T> T getValue() {
+    return (T) value;
+  }
+
+  public <T> void setValue(T value) {
+    this.value = value;
+  }
 
 }
