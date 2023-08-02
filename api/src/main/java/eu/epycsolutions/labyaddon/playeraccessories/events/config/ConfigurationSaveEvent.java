73bbc4b8df50751063ec9e968dd916351a66fec3
EinsWurmi <wurmlitv@gmail.com>
2023-08-02 20:50:14 +0200

    Created ``ConfigurationSaveEvent``

---
+++ api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/events/config/ConfigurationSaveEvent.java
@@ -0,0 +1,7 @@
+package eu.epycsolutions.labyaddon.playeraccessories.events.config;
+
+import net.labymod.api.event.Event;
+import net.labymod.api.event.LabyEvent;
+
+@LabyEvent(background = true)
+public class ConfigurationSaveEvent implements Event { }
