793e900ace1bf3665cea11c72b7341d40e1ebe67
EinsWurmi <wurmlitv@gmail.com>
2023-08-03 21:59:25 +0200

    Removed change listener cause this file is just for implement

--- core/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/configuration/laby/PlayerAccessoriesConfig.java
+++ core/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/configuration/laby/PlayerAccessoriesConfig.java
@@ -7,9 +7,7 @@ import net.labymod.api.configuration.loader.property.ConfigProperty;
 @ConfigName("player-accessories")
 public class PlayerAccessoriesConfig extends AddonConfig {
 
-  private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true).addChangeListener((property, oldValue, newValue) -> {
-
-  });
+  private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
   @Override
   public ConfigProperty<Boolean> enabled() {
