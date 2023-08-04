f43705135601c47207da2377fb2ce99199aab0f2
EinsWurmi <wurmlitv@gmail.com>
2023-08-04 20:11:30 +0200

    Created dependency types

---
+++ api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/model/environ/info/dependency/AddonDependency.java
@@ -0,0 +1,3 @@
+package eu.epycsolutions.labyaddon.playeraccessories.model.environ.info.dependency;
+
+public record AddonDependency(String namespace, boolean optional) { }
---
+++ api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/model/environ/info/dependency/EnvironDependency.java
@@ -0,0 +1,3 @@
+package eu.epycsolutions.labyaddon.playeraccessories.model.environ.info.dependency;
+
+public record EnvironDependency(String namespace, boolean optional) { }
