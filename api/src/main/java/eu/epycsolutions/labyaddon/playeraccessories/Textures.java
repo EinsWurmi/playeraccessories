cd4b3bc0a53dc0a93adc514d5d66c9603c03d7a0
EinsWurmi <wurmlitv@gmail.com>
2023-10-13 11:01:49 +0200

    Created textures storage file

---
+++ api/src/main/java/eu/epycsolutions/labyaddon/playeraccessories/Textures.java
@@ -0,0 +1,15 @@
+package eu.epycsolutions.labyaddon.playeraccessories;
+
+import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.client.resources.texture.ThemeTextureLocation;
+
+public class Textures {
+
+  public static class SpriteLabyMod {
+    public static final ThemeTextureLocation TEXTURE = net.labymod.api.Textures.SpriteLabyMod.TEXTURE;
+
+    public static final Icon WHITE_WOLF_HIGH_RES = net.labymod.api.Textures.SpriteLabyMod.WHITE_WOLF_HIGH_RES;
+    public static final Icon DEFAULT_WOLF_HIGH_RES = net.labymod.api.Textures.SpriteLabyMod.DEFAULT_WOLF_HIGH_RES;
+  }
+
+}
